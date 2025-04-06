64185bfee3aa3e2ea0e49a1f85d62e83190a3043
codendive <codendive@gmail.com>
2025-04-06 16:00:31 +0200

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { stripe } from '@/lib/stripe'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
-const Home = () => {
-  return <div></div>
+const Home = async () => {
+  const products = await stripe.products.list({
+    expand: ['data.default_price'],
+    limit: 3
+  })
+
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to My Ecommerce</h2>
+            <p>Discover the latest products at the best prices.</p>
+            <Button asChild variant='default'>
+              <Link href='/products'>Browse All Products</Link>
+            </Button>
+          </div>
+          <Image
+            alt='Banner Image'
+            width={450}
+            height={450}
+            src={products.data[0].images[0]}
+          />
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default Home
